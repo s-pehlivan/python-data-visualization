879c3998db6f3a05d791759b5fdf312eaff55980
F. Sevde Pehlivan <fsevde.pehlivan@gmail.com>
2025-07-25 19:42:21 +0300

    Add json data visualization

---
+++ chapter-16/json-data/eq_explore_data.py
@@ -0,0 +1,39 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of data
+filename = 'data/eq_data_1_day_m1.json'
+
+with open(filename) as f:
+	all_eq_data = json.load(f)
+
+readable_file = 'data/readable_eq_data.json'
+
+with open(readable_file, 'w') as f:
+	json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+
+for eq_dict in all_eq_dicts:
+	mag = eq_dict['properties']['mag']
+	lon = eq_dict['geometry']['coordinates'][0]
+	lat = eq_dict['geometry']['coordinates'][1]
+	mags.append(mag)
+	lons.append(lon)
+	lats.append(lat)
+
+# Map The Earthquakes
+data = [{
+	'type': 'scattergeo',
+	'lon': lons,
+	'lat': lats
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename="global_earthquakes.html")
+ 
